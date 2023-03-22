450271e46a3aaa81065ee8fd580b284fb2f3f8a0
Rıdvan Sünger <rsunger@ozdisan.com>
2023-03-22 11:00:13 +0300

    localstorage

--- JavaScript/js/Odev1/js/counter.js
+++ JavaScript/js/Odev1/js/counter.js
@@ -1,4 +1,4 @@
-let counter=0;
+let counter=localStorage.getItem("counter") ? Number(localStorage.getItem("counter")):0;
 let counterDom=document.querySelector("#counter");
 let increaseDOM=document.querySelector("#increase");
 let decreaseDOM=document.querySelector("#decrease");
@@ -9,8 +9,6 @@ decreaseDOM.addEventListener("click",clickEvent);
 
 
 function clickEvent(){
-    if(this.id=="increase")
-        counterDom.innerHTML=counter+=1;
-    else if(this.id="decrease")
-        counterDom.innerHTML=counter-=1;
+   this.id=="increase" ? counterDom.innerHTML=counter+=1 : this.id="decrease" ? counterDom.innerHTML=counter-=1 : counter=0;
+   localStorage.setItem("counter",counter);
 }
