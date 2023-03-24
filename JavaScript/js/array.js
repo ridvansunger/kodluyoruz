b9edbfc9798225ff12237c16c740d806a17599cc
Rıdvan Sünger <rsunger@ozdisan.com>
2023-03-24 17:46:15 +0300

    Array-Loops

--- JavaScript/js/array.js
+++ JavaScript/js/array.js
@@ -55,3 +55,92 @@ for (const key in dizi3) {
 }
 console.log(factor);
 
+
+// 1-) break için basit örnek
+// normalde 1 den 10 a kadar yazdırması gerekirken 5 te break ile karşılaşıldığı
+//için dögüden çıkıldı hemen orda döngü sonlandırıldı
+for (var i = 0; i <= 10; i++) {
+    if (i == 5) {
+      break;
+    }
+    console.log("i:" + i);
+   }
+   console.log("break Döngüden çıkıldı");
+   
+   //2-) continue için basit örnek
+   // 1 den 10 a kadar yazıldı fakat 5 te continue ile karşılaşıldığı için o anki
+   //işlem olan 5 sayısı  pas geçilerek direk 6 ya geçildi
+   for (var i = 0; i <= 10; i++) {
+    if (i == 5) {
+      continue;
+    }
+    console.log("i:" + i);
+   }
+   console.log("Döngüden çıkıldı");
+
+
+
+//İç içe döngü örneği
+//  iç döngü de 3 değerinde break ifadesi çalışır ve o anda iç döngüden çıkılır yani
+//iç döngü sonlanır.Fakat dış döngünün işleyişi aynen devam eder
+ for (var i = 0; i <= 10; i++) {
+     console.log("dış döngü: " + i);
+     for (var j = 0; j <= 5; j++) {
+       if (j == 3) {
+         break;
+       }
+       console.log("iç döngü" + j);
+    }
+ }
+
+///break continue
+//Etiketli break Örneği
+//etiketli break ifadesi her ne kadar iç döngü içerisinde olsa dahi
+//etiketin başına konulduğu döngü için etki eder.
+//Böylece daha geniş çaplı bir dögüden çıkılmış oldu.
+cikis_etiketi: for (var i = 0; i <= 5; i++) {
+    for (var j = 0; j <= 50; j++) {
+      if (j == 15) {
+        break cikis_etiketi;
+      }
+      console.log("iç döngüden j :" + j);
+    }
+   }
+
+
+
+  
+
+   ///While
+var degisken = 1;
+while (degisken <= 10){
+ 
+ console.log ("degiskenin degeri: " + degisken ) 
+  
+ degisken++;
+}
+
+///Sorular
+//Filter
+const person = [{
+    name: "Adem",
+    age: 25,
+    languages: ["JavaScript", "CSS"],
+  },
+  {
+    name: "Oğuz",
+    age: 33,
+    languages: ["Java", "HTML"],
+    }
+  ];
+
+ // Soru 1: Yukarıdaki diziyi kullanarak filter() metodu ile yaşı 30'dan büyük olan kişiyi getirin.
+ const personBigger30= person.filter(p=>p.age>30);
+ personBigger30.forEach(item=>{
+    console.log(item);
+ })
+//Soru 2: Yukarıdaki diziyi kullanarak filter() metodu ile JavaScript bilen kişiyi getirin.
+const knowJavaScript=person.filter(p=>p.languages.includes("JavaScript"));
+knowJavaScript.forEach(item=>{
+    console.log(item);
+})
